0e4d32882c53fc55fdb8fa2c1ae846b489082dbe
Tarun Krishna <112946269+Mtarunkrishna@users.noreply.github.com>
2022-12-29 11:04:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import TopDrawer from "./components/elements/Drawers";
+import "./styles/global.css";
+
+function App() {
+  return (
+    <div className="App">
+      <TopDrawer />
+      <header className="App-header">
+        <p>
+          Assignment 2 by <code style={{ color: "orange" }}>Team5</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
